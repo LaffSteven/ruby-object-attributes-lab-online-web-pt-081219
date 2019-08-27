019908eb3bada36200394ca2502935c6f64dfe47
Steven Laff <steve.laff@gmail.com>
2019-08-27 03:11:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
   
   def name=(dog_name)
-    @my_name = dog_name
+    @name = dog_name
   end
   
   def name
-    @my_name
+    @name
   end
   
   def breed=(dog_breed)
-    @my_breed = dog_breed
+    @breed = dog_breed
   end
   
   def breed
-    @my_breed
+    @breed
   end
   
 end
