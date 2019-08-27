9edd8371fa78b92f23bd663bd775a35973ae57fe
Steven Laff <steve.laff@gmail.com>
2019-08-27 03:10:31 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(a_name)
+    @my_name = a_name
+  end
+  
+  def name
+    @my_name
+  end
+  
+  def job=(a_job)
+    @my_job = a_job
+  end
+  
+  def job
+    @my_job
+  end
+  
+end
